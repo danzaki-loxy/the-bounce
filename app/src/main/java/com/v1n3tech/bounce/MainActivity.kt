722812b6f616c6c7aeed70b6a38d04fca1b1b810
know-one-God <henessyxander@gmail.com>
2025-10-31 08:44:55 +0100

    When all fragments were created and connected

--- app/src/main/java/com/v1n3tech/bounce/MainActivity.kt
+++ app/src/main/java/com/v1n3tech/bounce/MainActivity.kt
@@ -5,8 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.v1n3tech.bounce.fragments.ExploreFragment
+import com.v1n3tech.bounce.fragments.HomeFragment
+import com.v1n3tech.bounce.fragments.LibraryFragment
+import com.v1n3tech.bounce.fragments.WalletFragment
+import me.ibrahimsn.lib.SmoothBottomBar
 
 class MainActivity : AppCompatActivity() {
+
+    private val homeFragment = HomeFragment()
+    private val exploreFragment = ExploreFragment()
+    private val libraryFragment = LibraryFragment()
+    private val walletFragment = WalletFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +27,26 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val bottomBar = findViewById<SmoothBottomBar>(R.id.bottomBar)
+        replaceFragment(homeFragment)
+
+        bottomBar.onItemSelected = {
+            when (it) {
+                0 -> replaceFragment(homeFragment)
+                1 -> replaceFragment(exploreFragment)
+                2 -> replaceFragment(libraryFragment)
+                3 -> replaceFragment(walletFragment)
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            setCustomAnimations(R.anim.fade_in, R.anim.fade_out)
+            replace(R.id.frame, fragment)
+            commit()
         }
     }
 }
---
+++ app/src/main/java/com/v1n3tech/bounce/fragments/ExploreFragment.kt
@@ -0,0 +1,18 @@
+package com.v1n3tech.bounce.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.v1n3tech.bounce.R
+
+class ExploreFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_explore, container, false)
+    }
+}
